02d22f0d92b31f5d2a2a220221710b32b8ce4e4c
Arwa alhomrani <lla644183@gmail.com>
2026-07-11 02:11:51 +0300

    Refactor print_times_table for better output formatting
    Updated the print_times_table function to improve formatting and handle edge cases for numbers up to 100.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,51 +1,40 @@
-/*
- * File: 100-times_table.c
- * Auth: Lalo Rdz
- */
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table.
- * @n: First printed number
- *
- * Description: Prints the n times table, starting with 0.
- * Return: void
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	int i, j, aux;
+	int i, j, prod;
 
-	if (n >= 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(48);
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				aux = i * j;
-				if (aux >= 100)
-				{
-					_putchar((aux / 100)  + 48);
-					aux = aux % 100;
-					_putchar((aux / 10)  + 48);
-					_putchar((aux % 10)  + 48);
-				}
-				else
-				{
+
+				prod = i * j;
+
+				if (prod <= 9)
 					_putchar(' ');
-					if (aux >= 10)
-					{
-						_putchar((aux / 10) + 48);
-						_putchar((aux % 10) + 48);
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(aux + 48);
-					}
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
 				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
