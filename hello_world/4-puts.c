c090c975084bb1e9e11ab680a7df0253fc7ed47c
Arwa alhomrani <lla644183@gmail.com>
2026-07-10 23:38:43 +0300

    Add files via upload

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - prints a message using hte function puts that outputs plain text
+*
+*instead of printf.
+*
+* Return: a 0 value confirming the correct run of the code
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
