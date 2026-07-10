74519a676216c70ff722df5c4e6e6b1f933b7309
Arwa alhomrani <lla644183@gmail.com>
2026-07-11 02:24:16 +0300

    Improve Fibonacci sequence calculation
    Refactor Fibonacci calculation to handle large numbers correctly.

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,6 @@ int main(void)
 	unsigned long int f1_h, f1_t, f2_h, f2_t, next_h, next_t;
 
 	printf("%lu, %lu", f1, f2);
-
 	for (i = 3; i <= 91; i++)
 	{
 		next = f1 + f2;
@@ -20,23 +19,19 @@ int main(void)
 		f1 = f2;
 		f2 = next;
 	}
-
 	f1_h = f1 / 1000000000;
 	f1_t = f1 % 1000000000;
 	f2_h = f2 / 1000000000;
 	f2_t = f2 % 1000000000;
-
 	for (i = 92; i <= 98; i++)
 	{
 		next_h = f1_h + f2_h;
 		next_t = f1_t + f2_t;
-		
 		if (next_t >= 1000000000)
 		{
 			next_h += 1;
 			next_t %= 1000000000;
 		}
-		
 		printf(", %lu%09lu", next_h, next_t);
 		f1_h = f2_h;
 		f1_t = f2_t;
@@ -44,6 +39,5 @@ int main(void)
 		f2_t = next_t;
 	}
 	printf("\n");
-
 	return (0);
 }
