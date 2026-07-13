a9655bc529ac91ae4867e1a4811b2a2c119a614f
Arwa alhomrani <lla644183@gmail.com>
2026-07-14 01:44:11 +0300

    Update 104-print_buffer.c

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -18,22 +18,25 @@ void print_buffer(char *b, int size)
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x:", i);
-		for (j = i; j < i + 10; j += 2)
+		for (j = 0; j < 10; j++)
 		{
-			if (j < size && j + 1 < size)
-				printf(" %02x%02x", (unsigned char)b[j], (unsigned char)b[j + 1]);
-			else if (j < size)
-				printf(" %02x", (unsigned char)b[j]);
+			if (j % 2 == 0)
+				printf(" ");
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
 			else
-				printf("     ");
+				printf("  ");
 		}
 		printf(" ");
-		for (j = i; j < i + 10 && j < size; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
+			if (i + j < size)
+			{
+				if (b[i + j] >= 32 && b[i + j] <= 126)
+					printf("%c", b[i + j]);
+				else
+					printf(".");
+			}
 		}
 		printf("\n");
 	}
