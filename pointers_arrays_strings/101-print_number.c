95e4e9603c3edc6dd5e789fa056b2d10077f1a55
Arwa alhomrani <lla644183@gmail.com>
2026-07-14 01:15:37 +0300

    Add print_number function to print integers
    Implement print_number function to print integers recursively.

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,45 +1,28 @@
-Tasks
-9. Numbers have life; they're not just symbols on paper
-#advanced
-Write a function that prints an integer.
-
-Prototype: void print_number(int n);
-You can only use _putchar function to print
-You are not allowed to use long
-You are not allowed to use arrays or pointers
-You are not allowed to hard-code special values
-julien@ubuntu:~/$ cat 101-main.c
 #include "main.h"
 
 /**
- * main - check the code
+ * print_number - prints an integer
+ * @n: integer to be printed
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+void print_number(int n)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
-}
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 101-main.c 101-print_number.c -o 101-print_numbers
-julien@ubuntu:~/$ ./101-print_numbers 
-98
-402
-1024
-0
--98
-julien@ubuntu:~/$ 
-Repo:
+	unsigned int i;
 
-GitHub repository: holbertonschool-low_level_programming
-Directory: pointers_arrays_strings
-File: 101-print_number.c
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
