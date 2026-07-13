a6e914cac3540193f7ab21ffcf9c3e33b8d0a8b6
Arwa alhomrani <lla644183@gmail.com>
2026-07-13 11:14:48 +0300

    Update baseline_loop.c

--- benchmarking/baseline_loop.c
+++ benchmarking/baseline_loop.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <time.h>
 
+int main(void)
+{
+    clock_t start, end;
+    double cpu_time_used;
+    long i;
+
+    start = clock();
+
+    /* هنا نضع الكود الذي نريد قياسه */
+    for (i = 0; i < 100000000; i++)
+    {
+        /* عملية حسابية بسيطة */
+    }
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Execution time: %f seconds\n", cpu_time_used);
+    return (0);
+}
