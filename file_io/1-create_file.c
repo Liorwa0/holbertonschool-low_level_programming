965a285c60f999b465d48b8f1ef18bffb46e8be4
Arwa alhomrani <lla644183@gmail.com>
2026-07-20 02:37:31 +0300

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,46 +1,36 @@
-Tasks
-1. Under the snow
-Create a function that creates a file.
-
-Prototype: int create_file(const char *filename, char *text_content);
-where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
-Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write "fails", etc…)
-The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
-if the file already exists, truncate it
-if filename is NULL return -1
-if text_content is NULL create an empty file
-julien@ubuntu:~/File descriptors and permissions$ cat 1-main.c
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
  *
- * Return: Always 0.
+ * Return: 1 on success, -1 on failure
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-    int res;
+	int fd, n_write, len = 0;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (filename == NULL)
+		return (-1);
+
+	/* Create file with rw------- permissions */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		n_write = write(fd, text_content, len);
+		if (n_write == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
-julien@ubuntu:~/File descriptors and permissions$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-create_file.c -o b
-julien@ubuntu:~/File descriptors and permissions$ ./b hello world
--> 1)
-julien@ubuntu:~/File descriptors and permissions$ ls -l hello
--rw------- 1 julien julien 5 Dec  3 14:28 hello
-julien@ubuntu:~/File descriptors and permissions$ cat hello 
-worldjulien@ubuntu:~/File descriptors and permis$ 
-Repo:
-
-GitHub repository: holbertonschool-low_level_programming
-Directory: file_io
-File: 1-create_file.c
