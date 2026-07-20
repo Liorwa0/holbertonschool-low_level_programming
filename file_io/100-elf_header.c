3aa13d56a7389e6711da28ab40bddd3ad6695318
Arwa alhomrani <lla644183@gmail.com>
2026-07-20 03:00:30 +0300

    Update 100-elf_header.c

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -4,53 +4,56 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/* دوال مساعدة لطباعة الحقول (تحتاجين لتعريفها) */
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-/* ... وبقية الدوال ... */
+/**
+ * print_magic - prints magic bytes
+ * @e_ident: pointer to magic bytes
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int i;
 
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-    int fd;
-    Elf64_Ehdr header; /* استخدمي الهيكل المناسب */
+	int fd;
+	Elf64_Ehdr header;
 
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-        exit(98);
-    }
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
-        exit(98);
-    }
-
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read ELF header\n");
-        close(fd);
-        exit(98);
-    }
-
-    /* تحققي أن الملف هو ELF فعلاً */
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 || 
-        header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-        close(fd);
-        exit(98);
-    }
-
-    /* ابدئي بطباعة الحقول المطلوبة بالتسلسل */
-    printf("ELF Header:\n");
-    print_magic(header.e_ident);
-    /* ... استدعي باقي دوال الطباعة ... */
-
-    close(fd);
-    return (0);
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(98);
+	}
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read ELF header\n");
+		close(fd);
+		exit(98);
+	}
+	if (header.e_ident[EI_MAG0] != ELFMAG0)
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		close(fd);
+		exit(98);
+	}
+	printf("ELF Header:\n");
+	print_magic(header.e_ident);
+	/* هنا يجب إكمال بقية الدوال (Class, Data, Version, etc) بنفس نمط print_magic */
+	close(fd);
+	return (0);
 }
