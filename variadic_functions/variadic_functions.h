d1305c452eb6cac2691578bad6572cf39253ff1d
Arwa alhomrani <lla644183@gmail.com>
2026-07-27 23:07:21 +0300

    Add printer struct to variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,9 +4,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct printer - Struct printer
+ * @symbol: format symbol
+ * @print: function pointer to print corresponding type
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
